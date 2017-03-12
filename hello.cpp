914ad5b976ef6dfe5c25efe74424d75bdfdd9e6c
Fajar Winata <fajarwinata@hotmail.com>
2017-03-12 15:56:21 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+/*
+* Hello World Starting for CPP
+* Created By Fajar Winata
+* 12/03/2017
+* Inforfatics Engineering STT Bandung
+* NIM. 16.111.195
+* TIF 16 C RM
+*/
+
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+
+cout<<"Hello World\n";
+cout<<"TIF 16 RM C\n\n";
+cout<<"Fajar Winata NIM. 16.111.195";
+
+
+}
