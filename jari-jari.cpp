8388e4031bfb10b60ba04a1166037c98a3db0713
Fajar Winata <fajarwinata@hotmail.com>
2017-03-14 20:39:33 +0700

    Add files via upload

---
+++ jari-jari.cpp
@@ -0,0 +1,31 @@
+/*
+* LUAS LINGKARAN
+* Created By Fajar Winata
+* 12/03/2017
+* Inforfatics Engineering STT Bandung
+* NIM. 16.111.195
+* TIF 16 C RM
+*/
+
+#include<iostream>
+#define phi 3.14
+using namespace std;
+
+
+int main(){
+
+
+float jari_jari, luas;
+
+jari_jari=2.5; 
+
+luas= 0.5 * phi * jari_jari * jari_jari;
+
+
+cout<<"Data Lingkaran"<<endl;
+cout<<"N Luas:"<<jari_jari;
+cout<<"\n";
+
+return 0;
+
+}
---
+++ ukuran.cpp
@@ -0,0 +1,23 @@
+/*
+* SIZE OF
+* Created By Fajar Winata
+* 12/03/2017
+* Inforfatics Engineering STT Bandung
+* NIM. 16.111.195
+* TIF 16 C RM
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+cout<< "Ukuran char : "<< sizeof(char) << endl;
+cout<< "Ukuran int : "<< sizeof(int) << endl;
+cout<< "Ukuran short int : "<< sizeof(short int) << endl;
+cout<< "Ukuran long int : "<< sizeof(long int) << endl;
+cout<< "Ukuran float : "<< sizeof(float) << endl;
+cout<< "Ukuran double : "<< sizeof(double) << endl;
+cout<< "Ukuran wchar_t : "<< sizeof(wchar_t) << endl;
+return 0;
+
+}
